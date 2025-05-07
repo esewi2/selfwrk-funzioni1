95b0105aa8fea420aa9f10373a0f8f1ac02b9673
esewi osemwegie <kulboy900@gmail.com>
2025-05-07 07:36:31 +0200

    selfwork funzioni1

---
+++ main.js
@@ -0,0 +1,29 @@
+
+//selfwork-funzioni1
+
+function Giocodadi(tiri) {
+    
+    let player1 = 0;
+    let player2 = 0;
+
+    for (let i = 0; i < tiri; i++) {
+        let player1Roll = Math.floor(Math.random()*6)+1;
+        let player2Roll = Math.floor(Math.random()*6)+1;
+
+        player1 = player1 + player1Roll
+        player2 = player2 + player2Roll   
+    }
+
+    if (player1 > player2) {
+        
+        return `player 1 ha vinto con un punteggio pari a ${player1}`
+    }else if(player2 > player1){
+        return `player 2 ha vinto con un puinteggio pari a ${player1}`
+    }else {
+        return `pareggio con un punteggio di ${player1}`
+    }
+}
+
+let tiri = Number(prompt('quanti tiri devono effettuare i giocatori:?'))
+let result = Giocodadi(tiri)
+console.log(result);
